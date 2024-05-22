958b24e2b872ff95a24fc7334563f5e9bfb3f270
eliavem <64979576+eliavem@users.noreply.github.com>
2024-05-22 13:11:47 -0700

    set up db

---
+++ server.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
+
+dotenv.config({ path: "./config.env" });
+
+// if connected to database with password
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log("DB connections Succesful");
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
