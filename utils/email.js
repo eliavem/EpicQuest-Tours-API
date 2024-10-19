a87c66f13972ba1dd1d6bf8342046cb15e48a149
eliavem <64979576+eliavem@users.noreply.github.com>
2024-10-19 09:15:06 -0700

    Refactor Mail js into a Class

--- utils/email.js
+++ utils/email.js
@@ -1,24 +1,53 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async options => {
-  // Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split("")[0];
+    this.url = url;
+    this.from = `admin2 <${process.env.EMAIL_FROM}>`
+  }
+
+  newTransport() {
+    if(process.env.NODE_ENV === 'production') {
+      return 1;
     }
-  });
 
-  const mailOptions = {
-    from: "admin2 <admin2@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-  };
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
 
-  await transporter.sendMail(mailOptions);
-};
+  }
 
-module.exports = sendEmail;
+  //send the actual email
+  async send(template, subject) {
+    // console.log(`Sending email with subject: ${subject}`);
+    // Define Email Options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text: template
+    };
+
+    // console.log('mailOptions', mailOptions);
+    // Create Transporter and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('Welcome', 'Welcome to EpicTours!')
+  }
+
+  async sendPasswordReset(message) {
+    // console.log('in reset password method');
+    await this.send(message, 'Your password reset token (valid for 10 min)')
+  }
+
+}
